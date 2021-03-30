78a735dfe7ff86fd1ca27fc1a6e68935f46069e5
Omri Maoz <75577504+omrimaoz@users.noreply.github.com>
2021-03-30 03:44:37 +0300

    4th commit: 1. main - English capital letters recognition base on Keras dataset 2. Compressed Dataset of 104000 handwrite English letters images

--- handwrite_data.py
+++ handwrite_data.py
@@ -2,12 +2,12 @@
 from PIL import Image
 import numpy as np
 
-Handwrite_Dataset = np.zeros(shape =(62,30,40))
+# define variables
+Handwrite_Dataset = np.zeros(shape =(62*55,30,40)) # numpy 3'd array
 letters_digits = []
 dic = {}
 number =[]
 count = 0
-length=0
 for i in range(123):
     if 47<i<58 or 64<i<91 or 96<i<123:
         letters_digits.append(chr(i))
@@ -18,37 +18,26 @@ for i in range(123):
             number.append("00"+str(i))
         else:
             number.append("0" + str(i))
-count=0
-print(number)
+i=0
+j=0
 for x in letters_digits:
     for num in number[0:55]:
         if 64<ord(x)<91:
-            image = Image.open('Hnd/Img/'+x+"./img"+number[count]+"-"+num+".png").convert("L").resize((40,30))
+            image = Image.open('Hnd/Img/'+x+"./img"+number[i]+"-"+num+".png").convert("L").resize((40,30))
         else:
-            image = Image.open('Hnd/Img/'+x+"/img"+number[count]+"-"+num+".png").convert("L").resize((40,30))
-        length+=1
-    Handwrite_Dataset[count] = np.asarray(image)
-    print(count)
-    count+=1
+            image = Image.open('Hnd/Img/'+x+"/img"+number[i]+"-"+num+".png").convert("L").resize((40,30))
+        Handwrite_Dataset[j] = np.asarray(image)
+        j+=1
+    i+=1
+    print(x)
 
 Handwrite_Dataset_reshaped = Handwrite_Dataset.reshape(Handwrite_Dataset.shape[0],-1)
 np.savetxt("Handwrite_Dataset.txt",Handwrite_Dataset_reshaped)
 
 loaded_arr = np.loadtxt("Handwrite_Dataset.txt")
 
-# This loadedArr is a 2D array, therefore
-# we need to convert it to the original
-# array shape.reshaping to get original
-# matrice with original shape.
 load_original_arr = loaded_arr.reshape(
     loaded_arr.shape[0], loaded_arr.shape[1] // Handwrite_Dataset.shape[2], Handwrite_Dataset.shape[2])
 print(load_original_arr.shape)
-# data = np.asarray(image)
+print(Handwrite_Dataset.shape[2])
 
-# print(data.shape)
-#
-# print(image.format)
-# print(image.size)
-# print(image.mode)
-
-print(length)
