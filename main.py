4b1aee5d99440645cc77259d61c111fa420962db
Omri Maoz <75577504+omrimaoz@users.noreply.github.com>
2021-03-30 03:54:02 +0300

    Revert "4th commit: 1. main - English capital letters recognition base on Keras dataset 2. Compressed Dataset of 104000 handwrite English letters images"
    This reverts commit 78a735df

--- main.py
+++ main.py
@@ -2,66 +2,22 @@ import tensorflow as tf
 from tensorflow import keras  # high level API for machine learning. use tensorflow as backend
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+
 
 def show_image(image):
     plt.imshow(image, cmap=plt.cm.binary)  # plt.cm.binary for gray scale
     plt.show()
 
-def split_dataset(data):
-
-    K = int(data.shape[0]*0.6)
-    M = 100
-    train_images = np.empty(shape=(K*M, 30, 40))
-    train_labels = np.empty(shape=(K*M),dtype=np.int32)
-    test_images = np.empty(shape=(data.shape[0] -K, 30, 40))
-    test_labels = np.empty(shape=(data.shape[0] -K),dtype=np.int32)
-
-    index = [i for i in range(data.shape[0])]
-    random.shuffle(index)
-
-    train_index = index[:K]
-    test_index = index[K:]
-    for j in range(M):
-        i=0
-        for num in train_index:
-            train_images[K*j + i] = data[num]
-            train_labels[K*j + i] = num // 55  # TODO check if 55 is true
-            i += 1
-    i = 0
-    for num in test_index:
-        test_images[i] = data[num]
-        test_labels[i] = num // 55  # TODO check if 55 is true
-        i += 1
-    return train_images, train_labels, test_images, test_labels
-
 
 def main():
-    # data load - N=104000
     data = keras.datasets.fashion_mnist
-    data = np.loadtxt("Handwrite_Dataset.txt")  # dataset is 3d numpy array flatten into 2d array.
-    images_d = 28  # dataset images width
 
-    # reshape the data into 3d numpy array - shape=(3640,30,40)
-    # 3640 images constructed the dataset
-    # 30x40 is the image ratio
-    reshaped_data = data.reshape(
-        data.shape[0], data.shape[1] // images_d, images_d)
-    reshaped_data= reshaped_data[550:1980]
-    print(reshaped_data.shape)
     # define train and test data sets
-    train_images, train_labels, test_images, test_labels = split_dataset(reshaped_data)
-    print(train_images[0], train_labels[0])
+    (train_images, train_labels), (test_images, test_labels) = data.load_data()
+    print(train_images[0])
     # label names
-    # letter_digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    #                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    #                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    #                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    #                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    #                 ]
-    letter_digit = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-                    ]
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
     # work with values in the interval [0,1] to represent pixels
     train_images = train_images / 255.0
@@ -74,13 +30,12 @@ def main():
     # and an activation function - 'relu' - rectified linear unit.
     # layer 3 - a dense layer and the output layer, that means a fully connected layer with
     # 10 neurons and an activation function - 'softmax' - each neuron get a probability value
-    # and each neuron represent one of the clothing item define in letter_digit
+    # and each neuron represent one of the clothing item define in class_names
 
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(30, 40)),
+        keras.layers.Flatten(input_shape=(28, 28)),
         keras.layers.Dense(128, activation="relu"),
-        keras.layers.Dense(128, activation="relu"),
-        keras.layers.Dense(26, activation="softmax")
+        keras.layers.Dense(10, activation="softmax")
     ])
 
     # model parameters
@@ -88,22 +43,23 @@ def main():
 
     # model fitness function
     # epochs - define how many time the model see the train images from the data
-    model.fit(train_images, train_labels, epochs=8)
+    model.fit(train_images, train_labels, epochs=5)
 
     # test model accuracy
     result = model.evaluate(test_image, test_labels)
     print("Model Accuracy is: " + str(result[1] * 100)[0:3] + "%")
+
     # make a prediction - find it's index
     prediction = model.predict(test_images)
     for i in range(5):
         plt.grid(False)
         plt.imshow(test_images[i], cmap=plt.cm.binary)  # plt.cm.binary for gray scale
-        plt.xlabel("Actual: " + letter_digit[test_labels[i]])
-        plt.title("Prediction: " + letter_digit[np.argmax(prediction[i])])
+        plt.xlabel("Actual: " + class_names[test_labels[i]])
+        plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
         plt.show()
 
     # save the model
-    model.save("text_classification_model.h5")  # h5 is the extension in tf and keras
+    model.save("text_classification_model.h5") #h5 is the extension in tf and keras
 
 
 if __name__ == '__main__':
